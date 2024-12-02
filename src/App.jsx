e68fbb59b862d66bd4323511ba4322b7e6d82c79
dramirez <dramirezrrzz@gmail.com>
2024-12-01 18:42:56 -0600

    cargado los componentes dinamicamente con el Map

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
-          {data.map(() => {
-            return <Guitar />;
+          {data.map((guitar) => {
+            return <Guitar key={guitar.id} guitar={guitar} />;
           })}
         </div>
       </main>
