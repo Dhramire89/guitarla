25f23d492428de058a0e4f556caab887d1b4b0ad
dramirez <dramirezrrzz@gmail.com>
2024-12-08 10:41:52 -0600

    carrito cargado dinamicamente

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,32 @@ import { db } from "./data/data";
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists < 0) {
+      item.quantity = 1;
+      setCart([...cart, item]);
+      console.log("se agrego");
+    } else {
+      const updateCart = [...cart];
+      updateCart[itemExists].quantity++;
+      setCart(updateCart);
+      console.log("Ya existe...");
+    }
+  }
   return (
     <>
-      <Header />
+      <Header cart={cart} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
           {data.map((guitar) => {
-            return <Guitar key={guitar.id} guitar={guitar} />;
+            return (
+              <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+            );
           })}
         </div>
       </main>
